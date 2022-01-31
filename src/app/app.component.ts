27d09c693e70c050a649089c2f2a0f26e3d174ca
angelahb <angelahb1991@hotmail.com>
2022-01-31 15:33:10 -0300

    Actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Piano';
+
+  aplicarSonido(numero: number):void {
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/note' + numero + '.wav';
+    audio.load();
+    audio.play();
+  }
 }
+
